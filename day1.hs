129725c7f22ca6e0331208e3a0a6e8c246222886
syncrotron <shorovatin@live.ca>
2023-12-01 14:32:41 -0600

    Completed Day 1, Part 2

--- day1.hs
+++ day1.hs
@@ -8,12 +8,14 @@ import Text.Read (readMaybe)
 
 main = do
     args <- getArgs
-    handle <- openFile (head args) ReadMode -- Neet way to open filenames passed in as args
-    contents <- hGetContents handle -- Lazy; will only read file when needed. Can sut to buffer size with: hSetBuffering handle $ BlockBuffering (Just 2048)  
+    handle <- openFile (head args) ReadMode 
+    contents <- hGetContents handle 
     let filelines = words contents
-    print (sum (map (gen2DigitNum . condenseInts) filelines))
-    -- mapM_ print filelines
-    hClose handle -- Remember to close files when done
+    let ns = [("one",1::Int), ("two", 2), ("three", 3), ("four", 4), ("five", 5), ("six", 6), ("seven", 7), ("eight", 8), ("nine", 9)]
+    let test = "slconeightfoureight557m38"
+    --print (wordsToDigit ns test)
+    print (sum (map (gen2DigitNum . wordsToDigit ns) filelines))
+    hClose handle
 
 condenseInts :: String -> [Int]
 condenseInts xs = case xs of [] -> [] 
@@ -28,3 +30,20 @@ gen2DigitNum xs = case xs of [] -> 0
                              x:xs -> read (show x ++ show (last xs))
 
 -- End Part 1 --
+
+-- Start Part 2 --
+wordsToDigit :: [(String, Int)] -> String -> [Int]
+wordsToDigit ns xs = case xs of [] -> []
+                                [x] -> case readMaybe [x] :: Maybe Int of Nothing -> []
+                                                                          Just x' -> [x']
+                                x:xs -> case readMaybe [x] :: Maybe Int of Nothing -> helperWordToDigit ns (x:xs) ++ wordsToDigit ns xs
+                                                                           Just x'  -> x' : helperWordToDigit ns xs ++ wordsToDigit ns xs
+
+helperWordToDigit :: [(String, Int)] -> String -> [Int]
+helperWordToDigit ns xs = case ns of [] -> []
+                                                -- Bool logical compare of xs and numbers string name, multiplied by numbers value
+                                                -- (1 or 0) * (value of number) = (0) or (value of number)
+                                     ns -> case fromEnum (take (length (fst (head ns))) xs == fst (head ns)) * snd (head ns) of 
+                                        0 -> helperWordToDigit (tail ns) xs 
+                                        x -> [x]
+-- End Part 2 --
