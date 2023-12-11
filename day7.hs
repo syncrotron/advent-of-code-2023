6da37c60c915a364fee9d34bc839f363b97b4b60
syncrotron <shorovatin@live.ca>
2023-12-11 12:50:40 -0600

    Got working for example, but fails to produce correct for real input.

--- day7.hs
+++ day7.hs
@@ -8,10 +8,12 @@ import Data.Maybe
 import Data.Char
 import Text.Read ( readMaybe )
 import Data.List (sort)
+import Data.List (sortOn)
+import Data.Function (on)
+
 
 -- Start Part 1 --
--- Correct: 282277027
-
+-- Correct: 
 
 main = do
     args <- getArgs
@@ -19,97 +21,93 @@ main = do
     contents <- hGetContents handle
     -- All "lets" done for readability. Could be condensed into final call in brackets
     let fileLines = lines contents
-    print (parsedSortedHands [] fileLines)
+    let sortedHands = sortBy (handCompare) (fileLines) --Supposedly, sortBy is O(n log(n))
+    let sortedScores = map (drop 6) sortedHands
+    print sortedHands
+    print (sum (multiplyByIndex 1 (map (read :: String -> Int) sortedScores)))
     hClose handle -- Remember to close files when done
 
-data Cards = Two | Three | Four | Five | Six | Seven | Eight | Nine | Ten | Jack | Queen | King | Ace
-    deriving (Eq, Ord, Show, Read, Bounded, Enum)  
+multiplyByIndex :: Int -> [Int] -> [Int]
+multiplyByIndex mutiplyer iList = case iList of
+    [] -> []
+    i:is -> i*mutiplyer : multiplyByIndex (mutiplyer+1) is
 
+-- Rule 1 and Rule 2 Boolean check for if handA < handB
+handCompare :: String -> String -> Ordering
+handCompare handA handB = case (handA, handB) of
+    (handA, handB) -> if (handAScore < handBScore) then LT
+        else if (handAScore == handBScore) then cardComapre (take 5 handA) (take 5 handB)
+        else GT
+            where handAScore = handScorer handA
+                  handBScore = handScorer handB 
 
-parsedSortedHands :: [([Cards], Int)] -> [String] -> [([Cards], Int)]
-parsedSortedHands sortedHands lines = case lines of 
-    [] -> sortedHands
-    l:ls -> parsedSortedHands updatedHand ls
-        where cards = map charToCard (take 5 l)
-              score = read (last (words l))
-              updatedHand = insertSortHands sortedHands (cards, score)
+-- Rule 2 Boolean check for if handA < handB
+cardComapre :: String -> String -> Ordering
+cardComapre handA handB = case (handA, handB) of
+    (a:as, b:bs) -> if (cardAVal < cardBVal) then LT 
+        else if (cardAVal > cardBVal) then GT 
+        else cardComapre as bs
+        where cardAVal = charToCardStr a
+              cardBVal = charToCardStr b
+    (handA, []) -> LT
+    ([], handB) -> GT
 
-insertSortHands :: [([Cards], Int)] -> ([Cards], Int) -> [([Cards], Int)]
-insertSortHands handsList hand = case handsList of
-    [] -> [hand]
-    h:hs -> if listHandScore > inHandScore then hand:handsList
-            else if listHandScore == inHandScore then (sortColResolver h hand h hand) ++ hs
-            else h:insertSortHands hs hand ++ [h]
-        where listHandScore = handScorer (fst h)
-              inHandScore = handScorer (fst hand)
-
--- Dependant on 5 card hands 
-sortColResolver :: ([Cards], Int) -> ([Cards], Int) -> ([Cards], Int) -> ([Cards], Int) -> [([Cards], Int)]
-sortColResolver aHandOrg bHandOrg aHand bHand 
-    | null aCards || null bCards = [(fst aHandOrg, aScore+bScore)] -- This means the cards are equal
-    | (head aCards) > (head bCards) = aHandOrg:[bHandOrg]
-    | (head aCards) < (head bCards) = bHandOrg:[aHandOrg]
-    | otherwise = sortColResolver aHandOrg bHandOrg (tail aCards, aScore) (tail bCards, aScore)
-    where aCards = fst aHand
-          bCards = fst bHand
-          aScore = snd aHand
-          bScore = snd bHand
-
-charToCard :: Char -> Cards
-charToCard card = case card of
-    '2' -> Two
-    '3' -> Three
-    '4' -> Four
-    '5' -> Five
-    '6' -> Six
-    '7' -> Seven
-    '8' -> Eight
-    '9' -> Nine
-    'T' -> Ten
-    'J' -> Jack
-    'Q' -> Queen
-    'K' -> King
-    'A' -> Ace
+charToCardStr :: Char -> Int
+charToCardStr card = case card of
+    '2' -> (1::Int)
+    '3' -> 2
+    '4' -> 3
+    '5' -> 4
+    '6' -> 5
+    '7' -> 6
+    '8' -> 7
+    '9' -> 8
+    'T' -> 9
+    'J' -> 10
+    'Q' -> 11
+    'K' -> 12
+    'A' -> 13
     _ -> error "Unknown Char encountered. No Card mapping"
 
 -- Takes list of cards and scores them from 0-5 (Int)
-handScorer :: [Cards] -> Int
+handScorer :: String -> Int
 handScorer cards
     -- Gaurd order is important
-    | fiveOfKind cards = 6
-    | fourOfKind cards = 5
-    | fullHouse cards = 4
-    | threeOfKind cards = 3
-    | twoPair cards = 2
-    | onePair cards = 1   
-    | otherwise = 0
+    | fiveOfKind hand = 7
+    | fourOfKind hand = 6
+    | fullHouse hand = 5
+    | threeOfKind hand = 4
+    | twoPair hand = 3
+    | onePair hand = 2   
+    | otherwise = 1
+    where hand = (take 5 cards)
 
 -- All following hand types are dependant on hands being 5 cards long
-fiveOfKind :: [Cards] -> Bool
+fiveOfKind :: String -> Bool
 fiveOfKind cards = case (sort cards) of
     [a,_,_,_,e] -> a == e
 
-fourOfKind :: [Cards] -> Bool
+fourOfKind :: String -> Bool
 fourOfKind cards = case (sort cards) of
     [a,b,_,d,e] -> (a == d) || (b == e)
 
-fullHouse :: [Cards] -> Bool
+fullHouse :: String -> Bool
 fullHouse cards = case (sort cards) of
     [a,b,c,d,e] -> (a == b) && (c == e) ||
                    (a == c) && (d == e) 
 
-threeOfKind :: [Cards] -> Bool
+threeOfKind :: String -> Bool
 threeOfKind cards = case (sort cards) of
     [a,b,c,d,e] -> (a == b) && (b == c) ||
                    (c == d) && (d == e)
 
-twoPair :: [Cards] -> Bool
+twoPair :: String -> Bool
 twoPair cards = case (sort cards) of
     [a,b,c,d,e] -> (a == b) && (c == d) || 
                    (a == b) && (d == e) ||
                    (b == c) && (d == e) 
 
-onePair :: [Cards] -> Bool
+onePair :: String -> Bool
 onePair cards = case (sort cards) of
     [a,b,c,d,e] -> (a == b) || 
                    (b == c) || 
@@ -117,7 +115,7 @@ onePair cards = case (sort cards) of
                    (d == e)
 
 -- Would be redundant in 
-highCard :: [Cards] -> Bool
+highCard :: String -> Bool
 highCard cards =  case (sort cards) of
     [a,b,c,d,e] -> (a /= b) || 
                    (b /= c) || 
