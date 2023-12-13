c0d6a72d33c3fa7509506f951d84f0ad3e6cd777
syncrotron <shorovatin@live.ca>
2023-12-12 21:33:55 -0600

    Completed Day 6, Part 2

--- day6.hs
+++ day6.hs
@@ -7,7 +7,39 @@ import Data.Char
 
 -- Start Part 1 --
 -- Wrong: 
--- Correct: 
+-- Correct: 170000
+
+-- main = do
+--     args <- getArgs
+--     handle <- openFile (head args) ReadMode
+--     contents <- hGetContents handle
+--     -- All "lets" done for readability. Could be condensed into final call in brackets
+--     let fileLines = lines contents
+--     let intFileLines = map (map (read :: String -> Float) . tail . words) fileLines
+--     print (product (zipWith (curry quadraticSolution) (intFileLines !! 0) (intFileLines !! 1)))
+--     hClose handle -- Remember to close files when done
+
+-- -x^2 + xa - y = 0, x = speed in mm/s, y = distance (mm) travelled, a = Total race time (s)
+-- Take a tuple of (a, y), returns number of whole second solutions above record distance y
+quadraticSolution :: (Float, Float) -> Int
+quadraticSolution (a, y) =  (floor startX) - (floor endX)
+    where startX = (-a - sqrt (a*a - (4*(y+0.1)))) / (-2) -- Need to add small amount (.1) to y so I am not getting exact intercepts of record distance
+          endX = (-a + sqrt (a*a - (4*(y+0.1)))) / (-2)
+
+-- End Part 1 --
+
+-- Start Part 2 --
+-- Wrong: 20537784, ↑
+-- Correct: 20537782
+
+-- -x^2 + xa - y = 0, x = speed in mm/s, y = distance (mm) travelled, a = Total race time (s)
+-- Take a tuple of (a, y), returns number of whole second solutions above record distance y
+-- Needs precision of double for very large numbers 
+quadraticSolutionDouble :: (Double, Double) -> Int
+quadraticSolutionDouble (a, y) = floor (startX - endX)
+    where startX = (-a - sqrt (a*a - (4*(y+0.1)))) / (-2) -- Need to add small amount (.1) to y so I am not getting exact intercepts of record distance
+          endX = (-a + sqrt (a*a - (4*(y+0.1)))) / (-2)
+
 
 main = do
     args <- getArgs
@@ -15,15 +47,8 @@ main = do
     contents <- hGetContents handle
     -- All "lets" done for readability. Could be condensed into final call in brackets
     let fileLines = lines contents
-    let intFileLines = map (map (read :: String -> Float) . tail . words) fileLines
-    print (product (zipWith (curry quadraticSolution) (intFileLines !! 0) (intFileLines !! 1)))
+    let intFileLines = map ((read :: String -> Double) . concat . tail . words) fileLines
+    print ((quadraticSolutionDouble (intFileLines !! 0 , intFileLines !! 1)))
     hClose handle -- Remember to close files when done
 
--- -x^2 + xa - y = 0, x = speed in mm/s, y = distance (mm) travelled, a = Total race time (s)
--- Take a tuple of (a, y), returns number of whole second solutions aboev record distance y
-quadraticSolution :: (Float, Float) -> Int
-quadraticSolution (a, y) =  (floor startX) - (floor endX)
-    where startX = (-a - sqrt (a*a - (4*(y+0.1)))) / (-2) -- Need to add small amount (.1) to y so I am not getting exact intercepts of record distance
-          endX = (-a + sqrt (a*a - (4*(y+0.1)))) / (-2)
-
--- End Part 1 --
+-- End Part 2 --
